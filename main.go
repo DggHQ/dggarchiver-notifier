0359cb5c0d4c5a0bca4c38b73d70a1b718fa448d
vyneer <vyn33r@gmail.com>
2023-04-10 06:32:47 +0300

    chore: forgot to remove comments

--- main.go
+++ main.go
@@ -97,43 +97,5 @@ func main() {
 		}
 	}
 
-	// if cfg.Notifier.Platforms.YouTube.Enabled {
-	// 	if cfg.Notifier.Platforms.YouTube.APIRefresh != 0 {
-	// 		log.Infof("Checking YT API every %d minute(s)", cfg.Notifier.Platforms.YouTube.APIRefresh)
-	// 		sleepTime := time.Second * 60 * time.Duration(cfg.Notifier.Platforms.YouTube.APIRefresh)
-	// 		wg.Add(1)
-	// 		yt.StartYTThread("[YT] [API]", yt.LoopApiLivestream, &cfg, &state, sleepTime)
-	// 	}
-
-	// 	if cfg.Notifier.Platforms.YouTube.ScraperRefresh != 0 {
-	// 		log.Infof("Checking YT scraped page every %d minute(s)", cfg.Notifier.Platforms.YouTube.ScraperRefresh)
-	// 		sleepTime := time.Second * 60 * time.Duration(cfg.Notifier.Platforms.YouTube.ScraperRefresh)
-	// 		wg.Add(1)
-	// 		yt.StartYTThread("[YT] [SCRAPER]", yt.LoopScrapedLivestream, &cfg, &state, sleepTime)
-	// 	}
-	// }
-
-	// time.Sleep(1 * time.Second)
-
-	// if cfg.Notifier.Platforms.Rumble.Enabled {
-	// 	if cfg.Notifier.Platforms.Rumble.ScraperRefresh != 0 {
-	// 		log.Infof("Checking Rumble scraped page every %d minute(s)", cfg.Notifier.Platforms.Rumble.ScraperRefresh)
-	// 		sleepTime := time.Second * 60 * time.Duration(cfg.Notifier.Platforms.Rumble.ScraperRefresh)
-	// 		wg.Add(1)
-	// 		rumble.StartRumbleThread("[Rumble] [SCRAPER]", rumble.LoopScrapedLivestream, &cfg, &state, sleepTime)
-	// 	}
-	// }
-
-	// time.Sleep(1 * time.Second)
-
-	// if cfg.Notifier.Platforms.Kick.Enabled {
-	// 	if cfg.Notifier.Platforms.Kick.ScraperRefresh != 0 {
-	// 		log.Infof("Checking Kick scraped API every %d minute(s)", cfg.Notifier.Platforms.Kick.ScraperRefresh)
-	// 		sleepTime := time.Second * 60 * time.Duration(cfg.Notifier.Platforms.Kick.ScraperRefresh)
-	// 		wg.Add(1)
-	// 		kick.StartKickThread("[Kick] [SCRAPER]", kick.LoopScrapedLivestream, &cfg, &state, sleepTime)
-	// 	}
-	// }
-
 	wg.Wait()
 }
